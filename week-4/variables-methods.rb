a4ffd2d8c9a0a477fa7893188de15c308b3526c1
baronkwan <baronkwan@outlook.com>
2015-12-26 00:53:44 -0800

    Solve 4.3

---
+++ week-4/variables-methods.rb
@@ -0,0 +1,58 @@
+# Release 2: Full name greeting challenge
+
+puts "What is first name?"
+first_name = gets.chomp
+puts "What is your middle name?"
+middle_name = gets.chomp
+puts "What is your last name?"
+last_name = gets.chomp
+puts "Hello! #{first_name} #{middle_name} #{last_name}!"
+
+# Release 2: Bigger, better favorite number
+
+puts "What is your favorite number?"
+fav_num = gets.chomp.to_i + 1
+puts "A bigger and better number would be #{fav_num.to_s}"
+
+=begin
+	
+
+
+# my_solution.rb Ex. 4.3.1
+
+# defining_variable_spec.rb Ex 4.3.2
+
+=end
+
+
+
+# Release 6: Reflect
+
+=begin
+
+How do you define a local variable?
+
+	To define a local variable you need to create a name which is begin with a lowercase letter or _. then use the "="/equal sign to assign the value you want to the name you just created.
+
+How do you define a method?
+
+	To define a method you need to start off by typing the keyword "def". Then, the name of the method and parameters inside parenthesis if neccessary. You then will need to put a collection of codes underneath the first line. When you finish remember to put "end" on the next new line.
+
+What is the difference between a local variable and a method?
+
+	A local variable can use to store string, integer or float value, and we can treat it as an object. A method is a block of codes and it is used by an object.
+
+How do you run a ruby program from the command line?
+
+	To run a ruby program from the command line, simply put "ruby " in front of the ruby file name.
+
+How do you run an RSpec file from the command line?
+
+	To run a Rspec file, simply put "rspec " in front of the spec ruby file name.
+
+
+What was confusing about this material? What made sense?
+
+	It was more like a review to me since I have touch on these thing before joining DBC. It was a great materials and it did give me a new point of view to see these whole "method-object" idea. The only thing that need to be mention would be the instruction is a bit confusing, not sure where to put the mini-challenge ruby files.
+
+=end
