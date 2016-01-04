adb691a19c72cfe68f9cc3307386821243e0135c
baronkwan <baronkwan@outlook.com>
2016-01-03 16:32:02 -0800

    Solve 4.5

---
+++ week-4/simple-substrings/my_solution.rb
@@ -0,0 +1,14 @@
+# Simple Substrings
+
+# I worked on this challenge [with: Alex Forger].
+
+
+# Your Solution Below
+
+def welcome(address)
+	if address.include?("CA")
+		"Welcome to California"
+	else
+		"You should move to California"
+	end
+end
