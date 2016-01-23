fb72fd83e19d748cc9cc5da0767dcd5ac5356881
baronkwan <baronkwan@outlook.com>
2016-01-22 20:59:11 -0800

    Solve GPS 3.1

--- week-7/gps6/my_solution.rb
+++ week-7/gps6/my_solution.rb
@@ -10,20 +10,22 @@ require_relative 'state_data'
 
 class VirusPredictor
 
+  # To initialize an instance object and storing three instance variables when called
   def initialize(state_of_origin, population_density, population)
     @state = state_of_origin
     @population = population
     @population_density = population_density
   end
 
-  def virus_effects
-    predicted_deaths(@population_density, @population, @state)
-    speed_of_spread(@population_density, @state)
+  # A helper method to allow us to call two private methods when calledxw
+  def virus_effects 
+    predicted_deaths
+    speed_of_spread
   end
 
   private
 
-  def predicted_deaths(population_density, population, state)
+  def predicted_deaths
     # predicted deaths is solely based on population density
     if @population_density >= 200
       number_of_deaths = (@population * 0.4).floor
@@ -41,7 +43,7 @@ class VirusPredictor
 
   end
 
-  def speed_of_spread(population_density, state) #in months
+  def speed_of_spread #in months
     # We are still perfecting our formula here. The speed is also affected
     # by additional factors we haven't added into this functionality.
     speed = 0.0
@@ -69,19 +71,52 @@ end
 # DRIVER CODE
  # initialize VirusPredictor for each state
 
+STATE_DATA.each do |state_name, state_information|
+  current_state = VirusPredictor.new(state_name, state_information[:population_density], 
+                  state_information[:population])
+  current_state.virus_effects
+end
 
-alabama = VirusPredictor.new("Alabama", STATE_DATA["Alabama"][:population_density], STATE_DATA["Alabama"][:population])
-alabama.virus_effects
+# alabama = VirusPredictor.new("Alabama", STATE_DATA["Alabama"][:population_density], STATE_DATA["Alabama"][:population])
+# alabama.virus_effects
 
-jersey = VirusPredictor.new("New Jersey", STATE_DATA["New Jersey"][:population_density], STATE_DATA["New Jersey"][:population])
-jersey.virus_effects
+# jersey = VirusPredictor.new("New Jersey", STATE_DATA["New Jersey"][:population_density], STATE_DATA["New Jersey"][:population])
+# jersey.virus_effects
 
-california = VirusPredictor.new("California", STATE_DATA["California"][:population_density], STATE_DATA["California"][:population])
-california.virus_effects
+# california = VirusPredictor.new("California", STATE_DATA["California"][:population_density], STATE_DATA["California"][:population])
+# california.virus_effects
 
-alaska = VirusPredictor.new("Alaska", STATE_DATA["Alaska"][:population_density], STATE_DATA["Alaska"][:population])
-alaska.virus_effects
+# alaska = VirusPredictor.new("Alaska", STATE_DATA["Alaska"][:population_density], STATE_DATA["Alaska"][:population])
+# alaska.virus_effects
 
 
 #=======================================================================
 # Reflection Section
+
+# What are the differences between the two different hash syntaxes shown in the state_data file?
+
+# It is a nested hash which has two hashes as its value. The first layer which uses string as its key as the double quotes implied,
+# while the second layer both items used the symbol as its key value.
+
+
+# What does require_relative do? How is it different from require?
+
+# The require_relative allows us to connect two separate files.
+# require uses the current directory that you are running the program from.
+# while require_relative uses the directory of where that program itself resides.
+
+
+# What are some ways to iterate through a hash?
+
+# There are numerous way to iterate through a hash, we use #each in this challenge.
+
+
+# When refactoring virus_effects, what stood out to you about the variables, if anything?
+
+# Before this challenge, I was not aware that instance variables can be retreive in a indirective way.
+
+
+# What concept did you most solidify in this challenge?
+
+# I learned more on how to deal with a hashy hash and how instance variables can be pass through within methods.
+
